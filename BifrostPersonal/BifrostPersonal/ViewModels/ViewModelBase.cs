f13041bca63883157553f5ff78f127971fa054a6
Andreas Johansson <glanshammar70@gmail.com>
2024-09-18 21:31:33 +0200

    Initial commit

---
+++ BifrostPersonal/BifrostPersonal/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BifrostPersonal.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ BifrostPersonal/BifrostPersonal/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BifrostPersonal.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
